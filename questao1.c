c2b7f2c968a3061dcba9198f7f4ae128760c31cc
eduestrela <eduardostrelainfo@gmail.com>
2025-08-19 13:20:24 -0300

    Exercicio 1 - Lista 2

---
+++ questao1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, i, j, k;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = n; j > i; j--) {
+            printf(" ");
+        }
+        for (k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
